387555cf2b8c04bd37a8bb6b989def8b2266f5a3
Daniel Pereira <danipereiracodes@mac.local>
2022-04-29 18:57:20 +0200

    added server file

--- server.js
+++ server.js
@@ -7,12 +7,6 @@ require("dotenv").config();
 
 const app = express();
 
-const url =
-	"https://53423326-448f-4a94-856a-85ed1a1d610b-europe-west1.apps.astra.datastax.com/api/rest/v2/namespaces/tickets/collections/tasks";
-
-const token =
-	"AstraCS:PGoDGqcaUQHyINyOWCjIYgOu:84da8fac2f60923ffcc20fc031aa170dd7b225c883c0f6bb35539497052eac2f";
-
 app.post("/tickets", async (req, res) => {
 	const formData = req.body.formData;
 
@@ -20,13 +14,13 @@ app.post("/tickets", async (req, res) => {
 		method: "POST",
 		headers: {
 			Accepts: "application/json",
-			"X-Cassandra-Token": token,
+			"X-Cassandra-Token": process.env.TOKEN,
 			"Content-Type": "application/json",
 		},
 		data: formData,
 	};
 	try {
-		const response = await axios(url, options);
+		const response = await axios(process.env.URL, options);
 		res.status(200).json(response.data);
 	} catch (error) {
 		console.log(error);
